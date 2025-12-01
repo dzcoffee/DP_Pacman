f9032efbd191b76a25e8f60808b4e1afb90c1801
rahanaman <104880000+rahanaman@users.noreply.github.com>
2025-12-01 13:40:37 +0900

    Test : LevelManager case 분리

--- src/java/test/level/LevelSystemTest.java
+++ src/java/test/level/LevelSystemTest.java
@@ -1,9 +1,7 @@
 package test.level;
 
-import game.LevelUIPanel;
 import game.entities.Pacman;
 import game.entities.ghosts.Ghost;
-import game.ghostStates.GhostState;
 import game.level.FrightenAllCommand;
 import game.level.GameLevelData;
 import game.level.ILevelDataObserver;
@@ -31,7 +29,7 @@ public class LevelSystemTest {
     }
 
     @Test
-    public void levelManagerTest_LevelData() {
+    public void levelManagerTest_pacmanSpeed() {
         LevelManager levelManager = new LevelManager();
         class TestLevelDataObserver implements ILevelDataObserver {
             public GameLevelData data;
@@ -49,8 +47,44 @@ public class LevelSystemTest {
 
         Assertions.assertThat(observer.data.getPacmanSpeed()).isEqualTo(2.0f);
 
+    }
+
+    @Test
+    public void levelManagerTest_ghostSpeed() {
+        LevelManager levelManager = new LevelManager();
+        class TestLevelDataObserver implements ILevelDataObserver {
+            public GameLevelData data;
+
+            @Override
+            public void updateLevelData(GameLevelData data) {
+                this.data = data;
+            }
+        }
+        TestLevelDataObserver observer = new TestLevelDataObserver();
+
+        levelManager.registerObserver(observer);
+
         levelManager.decreaseGhostSpeed();
         Assertions.assertThat(observer.data.getGhostSpeed()).isEqualTo(0.5f);
+
+
+    }
+
+    @Test
+    public void levelManagerTest_pacmanLife() {
+        LevelManager levelManager = new LevelManager();
+        class TestLevelDataObserver implements ILevelDataObserver {
+            public GameLevelData data;
+
+            @Override
+            public void updateLevelData(GameLevelData data) {
+                this.data = data;
+            }
+        }
+        TestLevelDataObserver observer = new TestLevelDataObserver();
+
+        levelManager.registerObserver(observer);
+
 
         levelManager.increasePacmanLife();
         Assertions.assertThat(observer.data.getPacmanLife()).isEqualTo(2);
@@ -58,7 +92,33 @@ public class LevelSystemTest {
     }
 
     @Test
-    public void levelManagerTest_PacmanLife() throws Exception {
+    public void levelManagerTest_pacmanLifeDecrease() {
+        TestLevelManager levelManager = new TestLevelManager();
+        class TestLevelDataObserver implements ILevelDataObserver {
+            public GameLevelData data;
+
+            @Override
+            public void updateLevelData(GameLevelData data) {
+                this.data = data;
+            }
+        }
+        TestLevelDataObserver observer = new TestLevelDataObserver();
+
+        levelManager.registerObserver(observer);
+
+        Pacman pacman = new Pacman(0, 0);
+        pacman.registerObserver(levelManager);
+        Ghost ghost = new TestGhost(0, 0);
+
+        levelManager.increasePacmanLife();
+        ghost.switchChaseMode();
+        pacman.notifyObserverGhostCollision(ghost);
+        Assertions.assertThat(observer.data.getPacmanLife()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void levelManagerTest_pacmanLifeDecrease_exitGame() {
         class TestGhost extends Ghost {
 
             public TestGhost(int xPos, int yPos) {
@@ -85,17 +145,11 @@ public class LevelSystemTest {
 
         levelManager.registerObserver(observer);
 
-        levelManager.increasePacmanLife();
+
         Pacman pacman = new Pacman(0, 0);
         pacman.registerObserver(levelManager);
         Ghost ghost = new TestGhost(0, 0);
-        ghost.switchFrightenedMode();
-        pacman.notifyObserverGhostCollision(ghost);
-        Assertions.assertThat(observer.data.getPacmanLife()).isEqualTo(2);
 
-        ghost.switchChaseMode();
-        pacman.notifyObserverGhostCollision(ghost);
-        Assertions.assertThat(observer.data.getPacmanLife()).isEqualTo(1);
 
         ghost.switchChaseMode();
         pacman.notifyObserverGhostCollision(ghost);
@@ -105,5 +159,60 @@ public class LevelSystemTest {
 
     }
 
+    @Test
+    public void levelManagerTest_pacmanLifeDecrease_frighten() {
+        class TestGhost extends Ghost {
 
+            public TestGhost(int xPos, int yPos) {
+                super(xPos, yPos, "blinky.png");
+            }
+        }
+        class TestLevelManager extends LevelManager {
+            public boolean isExited = false;
+            @Override
+            protected void exitGame(){
+                isExited = true;
+            }
+        }
+        TestLevelManager levelManager = new TestLevelManager();
+        class TestLevelDataObserver implements ILevelDataObserver {
+            public GameLevelData data = new GameLevelData();
+
+            @Override
+            public void updateLevelData(GameLevelData data) {
+                this.data = data;
+            }
+        }
+        TestLevelDataObserver observer = new TestLevelDataObserver();
+
+        levelManager.registerObserver(observer);
+
+        Pacman pacman = new Pacman(0, 0);
+        pacman.registerObserver(levelManager);
+        Ghost ghost = new TestGhost(0, 0);
+
+
+        ghost.switchFrightenedMode();
+        pacman.notifyObserverGhostCollision(ghost);
+        Assertions.assertThat(observer.data.getPacmanLife()).isEqualTo(1);
+
+
+
+    }
+
+
+    private static class TestGhost extends Ghost {
+
+        public TestGhost(int xPos, int yPos) {
+            super(xPos, yPos, "blinky.png");
+        }
+    }
+
+    private static class TestLevelManager extends LevelManager {
+        public boolean isExited = false;
+        @Override
+        protected void exitGame(){
+            isExited = true;
+        }
+    }
 }
