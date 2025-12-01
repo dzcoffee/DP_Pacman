c537b6dac60450b1da379b9bd07c9a969b93d6b2
rahanaman <104880000+rahanaman@users.noreply.github.com>
2025-12-01 13:59:40 +0900

    test : ScoreManager 케이스 분리

--- src/java/test/scoremanager/ScoreManagerTest.java
+++ src/java/test/scoremanager/ScoreManagerTest.java
@@ -15,21 +15,29 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class ScoreManagerTest {
+    Pacman pacman;
+    PacGum pacGum;
+    SuperPacGum superPacGum;
+    Ghost ghost;
 
+
+    private static class TestGhost extends Ghost {
+        public TestGhost(int x, int y) {
+            super(x, y, "blinky.png");
+        }
+    }
+
+    @Before
+    public void setUp() {
+         pacman = new Pacman(0,0);
+         pacGum = new PacGum(0, 0);
+         superPacGum = new SuperPacGum(0, 0);
+         ghost = new TestGhost(0, 0);
+    }
 
     @Test
-    public void testScoreManager_observeScore() {
+    public void testScoreManager_pacGumEaten() {
         ScoreManager scoreManager = new ScoreManager();
-        Pacman pacman = new Pacman(0,0);
-        PacGum pacGum = new PacGum(0, 0);
-        SuperPacGum superPacGum = new SuperPacGum(0, 0);
-        class TestGhost extends Ghost {
-            public TestGhost(int x, int y) {
-                super(x, y, "blinky.png");
-            }
-        }
-        Ghost ghost = new TestGhost(0, 0);
-
 
         pacman.registerObserver(scoreManager);
 
@@ -38,20 +46,53 @@ public class ScoreManagerTest {
         pacman.notifyObserverPacGumEaten(pacGum);
         Assertions.assertThat(scoreManager.getScore()).isEqualTo(10);
 
-        pacman.notifyObserverSuperPacGumEaten(superPacGum);
-        Assertions.assertThat(scoreManager.getScore()).isEqualTo(110);
+    }
+    @Test
+    public void testScoreManager_superPacGumEaten() {
+        ScoreManager scoreManager = new ScoreManager();
 
-        pacman.notifyObserverGhostCollision(ghost);
-        Assertions.assertThat(scoreManager.getScore()).isEqualTo(110);
+        pacman.registerObserver(scoreManager);
+
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(0);
+
+        pacman.notifyObserverSuperPacGumEaten(superPacGum);
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(100);
+
+    }
+
+    @Test
+    public void testScoreManager_ghostCollision_frighten() {
+        ScoreManager scoreManager = new ScoreManager();
+
+        pacman.registerObserver(scoreManager);
+
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(0);
 
         ghost.switchFrightenedMode();
 
         pacman.notifyObserverGhostCollision(ghost);
-        Assertions.assertThat(scoreManager.getScore()).isEqualTo(610);
-
-
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(500);
 
     }
+
+    @Test
+    public void testScoreManager_ghostCollision_notFrighten() {
+        ScoreManager scoreManager = new ScoreManager();
+
+        pacman.registerObserver(scoreManager);
+
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(0);
+
+        ghost.switchChaseMode();
+
+        pacman.notifyObserverGhostCollision(ghost);
+        Assertions.assertThat(scoreManager.getScore()).isEqualTo(0);
+
+    }
+
+
+
+
     @Test
     public void testScoreManager_levelUpEvent() throws Exception {
         ScoreManager scoreManager = new ScoreManager();
@@ -66,7 +107,6 @@ public class ScoreManagerTest {
 
         verify(observer, times(1)).updateLevelUpEvent();
     }
-
 
 
 }
