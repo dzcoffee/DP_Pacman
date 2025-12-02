0db2943c030e15d261ff20a9ca369b8b99c49f57
Jeong <jeonginlee72@gmail.com>
2025-12-02 22:59:39 +0900

    refactor : UML 이쁘게 하기 위해 inputCommand를 abstract 클래스의 멤버변수로 변경
    - 안정성을 위해 지웠던 NullInputCommand 추가

--- src/java/game/keyInputManager/GameState.java
+++ src/java/game/keyInputManager/GameState.java
@@ -1,7 +1,10 @@
 package game.keyInputManager;
 
+import game.keyInputManager.command.InputCommand;
+import game.keyInputManager.command.NullInputCommand;
 import game.utils.KeyHandler;
 
 public abstract class GameState {
+    protected InputCommand inputCommand = new NullInputCommand();
     public void getInput(KeyHandler k) {};
 }
--- src/java/game/keyInputManager/PlayState.java
+++ src/java/game/keyInputManager/PlayState.java
@@ -1,21 +1,16 @@
 package game.keyInputManager;
 
-import game.GameplayPanel;
-import game.entities.Pacman;
 import game.keyInputManager.command.InputCommand;
-import game.keyInputManager.command.PacmanInputCommand;
 import game.utils.KeyHandler;
 
 public class PlayState extends GameState {
 
-    private InputCommand pacmanInputCommand;
-
     public PlayState(InputCommand pacmanInputCommand) {
-        this.pacmanInputCommand = pacmanInputCommand;
+        inputCommand = pacmanInputCommand;
     }
 
     @Override
     public void getInput(KeyHandler k) {
-        pacmanInputCommand.execute(k);
+        inputCommand.execute(k);
     }
 }
